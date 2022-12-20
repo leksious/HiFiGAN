39306ebfca90db4f79a481e7b767758387f83d40
leksious <72123355+leksious@users.noreply.github.com>
2022-12-20 06:15:35 +0300

    fix configs

---
+++ configs/__init__.py
@@ -0,0 +1,2 @@
+from .config import MPDConfig, MSDConfig, GeneratorConfig, MelSpectrogramConfig, TrainerConfig
+__all__ = ['MPDConfig', 'MSDConfig', 'GeneratorConfig', 'MelSpectrogramConfig', 'TrainerConfig']
